602584688ef6cfe8339aa02093e9ec88e908ccd3
tynguyen2k1 <nguyenvantybg01@gmail.com>
2021-11-25 15:12:56 +0700

    fix: config not working

--- lua/plugins/colorizer.lua
+++ lua/plugins/colorizer.lua
@@ -1,14 +1,9 @@
-require'colorizer'.setup()
+local present, color = pcall(require, "colorizer")
+if not present then
+  return
+end
 
-DEFAULT_OPTIONS = {
-    RGB      = true;        -- #RGB hex codes
-    RRGGBB   = true;        -- #RRGGBB hex codes
-    names    = true;        -- "Name" codes like Blue
-    RRGGBBAA = true;        -- #RRGGBBAA hex codes
-    rgb_fn   = true;        -- CSS rgb() and rgba() functions
-    hsl_fn   = true;        -- CSS hsl() and hsla() functions
-    css      = true;        -- Enable all CSS features: rgb_fn, hsl_fn, names, RGB, RRGGBB
-    css_fn   = true;        -- Enable all CSS *functions*: rgb_fn, hsl_fn
-    -- Available modes: foreground, background
-    mode     = 'background';    -- Set the display mode.
+color.setup {
+  "*",
+  css = { rgb_fn = true; }
 }
