766ce23621f0e672f9ee1e4d6954b9bc29a5db76
tynguyen2k1 <nguyenvantybg01@gmail.com>
2021-11-25 15:14:50 +0700

    update

--- init.lua
+++ init.lua
@@ -1,16 +1,17 @@
-require('settings') -- setting UI
-require('keybinds') -- setting keyboard
-require('plugins')  -- load plugin
+require('settings')                         -- setting UI
+require('keybinds')                         -- setting keyboard
+require('plugins')                          -- load plugin
 
 
-require('theme')                  -- load theme
-require('plugins.galaxyline.theme1')     -- galaxyline
+require('theme')                            -- load theme
+-- require('plugins.galaxyline.theme1')     -- galaxyline
+require('plugins.lualine')           -- lualine
 require('plugins.bufferline')
 require('plugins.treesitter')
-require('plugins.lsp-config')  -- config lsp installer
-require('plugins.nvim-cmp')       -- loading and config nvim cmp
-require('plugins.telescope')      -- load config telescope
-require('plugins.comment')        --
+require('plugins.lsp-installer')
+require('plugins.nvim-cmp')                 -- loading and config nvim cmp
+require('plugins.telescope')                -- load config telescope
+require('plugins.comment')                  --
 require('plugins.autopairs')
 require('plugins.gitsigns')
 require('plugins.nvimtree')
@@ -18,4 +19,3 @@ require('plugins.indent-blankline')
 require('plugins.terminal')
 require('plugins.vim-matchup')
 require('plugins.colorizer')
-require('plugins.lsp-installer')
