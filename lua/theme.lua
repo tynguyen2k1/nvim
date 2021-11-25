f391caabd41f2dfb7c5159c5cac25df14710eb44
tynguyen2k1 <nguyenvantybg01@gmail.com>
2021-11-25 15:13:24 +0700

    add: lualine bold

--- lua/theme.lua
+++ lua/theme.lua
@@ -1,7 +1,7 @@
 -- There are 3 styles: day, night and storm
 G.tokyonight_style = 'storm'
 G.tokyonight_italic_comments = true
-G.tokyonight_italic_keywords = false
+G.tokyonight_italic_keywords = true
 G.tokyonight_italic_functions = false
 G.tokyonight_italic_variables = false
 G.tokyonight_transparent = false
@@ -9,6 +9,7 @@ G.tokyonight_hide_inactive_statusline = false
 G.tokyonight_sidebars = {'qf', 'vista_kind', 'terminal', 'packer'}
 G.tokyonight_dark_sidebar = true
 G.tokyonight_dark_float = true
+G.tokyonight_lualine_bold = true
 
 -- Load the colorscheme
 vim.cmd[[colorscheme tokyonight]]
